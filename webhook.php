080868dd62e28e83cb56bcaf4ee451963b705eae
Rails Udemy Course <maurohmartinez@gmail.com>
2020-04-10 11:27:19 +0300

    Webhook set.

--- webhook.php
+++ webhook.php
@@ -1,9 +1,9 @@
 <?php
 // SDK de Mercado Pago
-require __DIR__ .  '/vendor/autoload.php';
+// require __DIR__ .  '/vendor/autoload.php';
 
 // Credenciales
-MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-090914-5c508e1b02a34fcce879a999574cf5c9-469485398');
+// MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-090914-5c508e1b02a34fcce879a999574cf5c9-469485398');
 
 // switch($_POST["type"]) {
 //     case "payment":
@@ -24,7 +24,7 @@ $file = 'webhooks.txt';
 // Open the file to get existing content
 $current = file_get_contents($file);
 // Append a new person to the file
-$current .= $_POST["id"] . "\n";
+$current .= $_POST["id"] ?? 'Nothing...' . "\n";
 // Write the contents back to the file
 file_put_contents($file, $current);
 
